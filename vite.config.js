9b5f3ad9f20ef6920edc79c553bfea3b9dd747e7
Higor Carneiro <higor12cs@gmail.com>
2025-05-24 13:15:45 -0300

    fix: fixing vite.config for production again

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,30 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     plugins: [
-        vue(),
         laravel({
-            input: ["resources/js/app.js"],
+            input: "resources/js/app.js",
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
-    resolve: {
-        alias: {
-            "@": "/resources/js",
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    adminlte: ["admin-lte/dist/js/adminlte.min.js"],
+                    jquery: ["jquery"],
+                    bootstrap: [
+                        "admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js",
+                    ],
+                },
+            },
         },
     },
     define: {
